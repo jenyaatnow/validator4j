2901e63a0f30a2c57f6c2beb4ce18f085e426d30
Evgeniy Surin <jenyaatnow@gmail.com>
2021-03-24 12:39:07 +0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+}
+
+group = "com.validator4j"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "validator4j"
+include("validator4j-core")
+include("validator4j-apt")
+include("validator4j-sample")
